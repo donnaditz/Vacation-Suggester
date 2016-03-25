bff022ac12a485abf6ee905c74843f41cf9d4dbe
Gigi Ditz <donnaditz@gmail.com>
2016-03-25 11:48:11 -0700

    add additional text to js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,19 @@ $("form#about-you").submit(function(event){
     var destination = $("input:radio[name=vacation]:checked").val();
 
     if (destination === "vacation1") {
-      $("#destination-result").empty().append("You should travel to: Wailea, Maui");
+      $("#destination-result").empty().append("You should travel to the Wailea Grand Resort on the Hawaiian Island of Maui. Set on 40 acres of tropical gardens bordering Wailea Beach, this luxe resort is next to Wailea Blue Golf Course and 6 miles from Ahihi Kinau Natural Area Reserve. All rooms have private terraces or balconies.There's a plush spa with 40 treatment rooms and a fitness center. Other amenities include a kids' club, multiple restaurants, 9 pools and a 2,000-ft river with waterslides.");
 
     } else if (destination === "vacation2") {
-      $("#destination-result").empty().append("You should travel to Cooper Island Beach Club"); 
+      $("#destination-result").empty().append("You should travel to Cooper Island Beach Club in the British Virgin Islands where you will find a family-owned eco-resort on the sandy shores of Manchioneel Bay accessible only by private boat. ");
 
     } else if (destination === "vacation3") {
-      $("#destination-result").empty().append("You should travel to New York City:");
+      $("#destination-result").empty().append("You should travel to New York City, home to the Empire State Building, Times Square, Statue of Liberty, Broadway and other iconic sites. It is a fast-paced, globally influential center of art, culture and finance.");
 
     } else if (destination === "vacation4") {
-      $("#destination-result").empty().append("You should travel to Breckenridge, Colorado:");
+      $("#destination-result").empty().append("You should travel to Breckenridge, Colorado, a town at the base of the Rocky Mountains Tenmile Range. It is prennially one of the most visited ski resorts in the western hemisphere.");
 
     } else  {
-      $("#destination-result").empty().append("You should travel to Bend, Oregon:");
+      $("#destination-result").empty().append("You should travel to Bend, Oregon for some of the most spectacular and beautiful hikes in the country, if not the world. Several cycling routes within the Three Sisters Scenic Bikeway represent the most scenic road cycling routes in Central Oregon.");
 
     }
 
